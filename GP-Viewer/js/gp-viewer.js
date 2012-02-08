3e7b1dea103f3fda7eac95f0a3133f8b87c8af6d
ErgallM <eaglemoor@gmail.com>
2012-02-08 16:32:12 +0400

    Добавлена верстка для отображения картинок.

---
+++ GP-Viewer/js/gp-viewer.js
@@ -0,0 +1,10 @@
+var GPViewer = new Class({
+    Implements: [Options],
+    options: {
+        image: null
+    },
+
+    initialize: function(options) {
+        this.setOptions(options);
+    }
+});
